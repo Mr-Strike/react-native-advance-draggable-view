5c3f1d04bca7a006a08ec3996572e3e537a3452f
Brahamdev <brahamdev.sharma@ebizontek.com>
2019-06-01 06:08:19 +0530

    drawable signal change

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ export default class component extends Component {
 
   closeDrawer = () => {
     this.startAnimation(-100, 0, this.state.finalPosition, null, this.state.initialPositon);
-    this.props.onRelease && this.props.onRelease(true); // only add this line if you need to detect if the drawer is up or not
+    this.props.onRelease && this.props.onRelease(false); // only add this line if you need to detect if the drawer is up or not
   }
 
   isAValidMovement = (distanceX, distanceY) => {
